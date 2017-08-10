38177b6073f2a912fe1d03485c3a481a67edeeee
liyongqiang <yong.qiang.li@163.com>
2017-08-10 11:31:35 +0800

    修改忽略文件

---
+++ src/EIM.Cache/CacheMapper.cs
@@ -0,0 +1,146 @@
+﻿using EIM.Cache.CacheManagers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EIM.Cache
+{
+    public class CacheMapper : ObjectMapper
+    {
+        public CacheMapper(CacheContainer cacheContainer)
+            : base()
+        {
+            this.CacheContainer = cacheContainer;
+        }
+
+        public CacheContainer CacheContainer { set; get; }
+
+        protected override bool Map(object source, Type resultType, out object result)
+        {
+            Type sourceType = source.GetType();
+            if (this.KeyToCache(source, resultType, out result))
+            {
+                return true;
+            }
+            else if (this.CacheToKey(source, resultType, out result))
+            {
+                return true;
+            }
+            else if (this.ModelToCache(source, resultType, out result))
+            {
+                return true;
+            }
+
+            return base.Map(source, resultType, out result);
+        }        
+
+        private bool KeyToCache(object source, Type resultType, out object result)
+        {
+            result = null;
+
+            if ((source is string || source is int) && !ReflectionHelper.IsIList(resultType))
+            {
+                if (this.CacheContainer.Contains(resultType))
+                {
+                    object key = source;
+                    result = this.CacheContainer.Get(key, resultType);
+                    return true;
+                }
+            }
+            else if (source is string && ReflectionHelper.IsIList(resultType))
+            {
+                Type resultItemType = ReflectionHelper.GetCollectionItemType(resultType);
+                if (this.CacheContainer.Contains(resultItemType))
+                {
+                    ICacheManager manager = this.CacheContainer.GetManager(resultItemType);
+                    string formatedKey = source as string;
+                    string[] keys = formatedKey.Split(',');
+                    result = Activator.CreateInstance(resultType);
+                    IList resultList = result as IList;
+                    foreach (string key in keys)
+                    {
+                        resultList.Add(manager.Get(key));
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool CacheToKey(object source, Type resultType, out object result)
+        {
+            result = null;
+            Type sourceType = source.GetType();
+
+            if (!ReflectionHelper.IsIList(sourceType) && (resultType == typeof(string) || resultType == typeof(int)))
+            {
+                if (resultType == typeof(int) && source is IIdProvider)
+                {
+                    result = (source as IIdProvider).Id;
+                    return true;
+                }
+                else if (resultType == typeof(string) && source is IGuidProvider)
+                {
+                    result = (source as IGuidProvider).Guid;
+                    return true;
+                }
+                else if (resultType == typeof(string) && source is ICodeProvider)
+                {
+                    result = (source as ICodeProvider).Code;
+                    return true;
+                }
+
+                return false;
+            }
+            else if (ReflectionHelper.IsIList<IIdProvider>(sourceType) && resultType == typeof(string))
+            {
+                List<object> keyList = new List<object>();
+                IList list = source as IList;
+                foreach (object obj in list)
+                {
+                    object key = key = (source as IIdProvider).Id;
+                    keyList.Add(key);
+                }
+                result = string.Join(",", keyList);
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool ModelToCache(object source, Type resultType, out object result)
+        {
+            result = null;
+            Type sourceType = source.GetType();
+
+            if ((ReflectionHelper.Is<IIdProvider>(sourceType) || ReflectionHelper.Is<ICodeProvider>(sourceType) || ReflectionHelper.Is<IGuidProvider>(sourceType))
+                && !this.CacheContainer.Contains(sourceType)
+                && this.CacheContainer.Contains(resultType))
+            {
+                if (source is IIdProvider)
+                {
+                    IIdProvider idProviderSource = source as IIdProvider;
+                    result = this.CacheContainer.Get(idProviderSource.Id, resultType);
+                    return true;
+                }
+                else if (source is IGuidProvider)
+                {
+                    IGuidProvider guidProviderSource = source as IGuidProvider;
+                    result = this.CacheContainer.Get(guidProviderSource.Guid, resultType);
+                    return true;
+                }
+                else if (source is ICodeProvider)
+                {
+                    ICodeProvider codeProviderSource = source as ICodeProvider;
+                    result = this.CacheContainer.Get(codeProviderSource.Code, resultType);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/EIM.Cache/CacheProviders/CacheProvider.cs
@@ -0,0 +1,147 @@
+﻿using EIM.Cache.CacheManagers;
+using EIM.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EIM.Cache
+{
+    public interface ICacheProvider
+    {
+        /// <summary>
+        /// 加载缓存
+        /// </summary>
+        void Load(bool newThread);
+    }
+
+    public abstract class CacheProvider<CacheType> : ICacheProvider
+            where CacheType : class
+    {
+        public CacheProvider(CacheContainer cacheContainer, params ICacheManager[] dependentManagers)
+        {
+            this._lock = new object();
+            this._loading = false;
+            this._watch = new Stopwatch();
+            this.CacheManager = cacheContainer.GetManager<CacheType>();
+            if (this.CacheManager == null)
+            {
+                throw new ArgumentNullException("this.CacheManager");
+            }
+            this.DependentManagers = new List<ICacheManager>();
+            if (dependentManagers != null)
+            {
+                this.DependentManagers.AddRange(dependentManagers);
+            }
+        }
+        object _lock;
+        bool _loading;
+        Stopwatch _watch;
+
+        public CacheManager<CacheType> CacheManager { private set; get; }
+
+        public List<ICacheManager> DependentManagers { private set; get; }
+
+        public virtual void Load(bool newThread)
+        {
+            if (newThread)
+            {
+                Thread realodThread = new Thread(this.Load);
+                realodThread.Start();
+            }
+            else
+            {
+                this.Load();
+            }
+        }
+
+        protected virtual void Load()
+        {
+            try
+            {
+
+                if (this._loading)
+                {
+                    ConsoleHelper.WriteLine(this.GetType().Name + "检测到重复加载请求。");
+                    return;
+                }
+                this._loading = true;
+
+                this.OnLoading();
+
+                List<CacheType> caches = this.GetCaches();
+
+                foreach (CacheType cache in caches)
+                {
+                    this.CacheManager.Add(cache);
+                }
+#if DEBUG
+                //测试模拟加载延迟
+                if (caches.Count < 500)
+                {
+                    Thread.Sleep(200);
+                }
+#endif
+
+                this.OnLoaded();
+
+            }
+            catch (Exception ex)
+            {
+                EIMLog.Logger.Error(ex.Message, ex);
+                throw;
+            }
+            finally
+            {
+                this._loading = false;
+            }
+        }
+
+        protected virtual void OnLoading()
+        {
+            this._watch.Start();
+            ConsoleHelper.WriteLine(this.GetType().Name + "正在加载数据......");
+
+            this.CacheManager.Clear();
+            this.CacheManager.Status = CacheStatus.Loading;
+
+            foreach (ICacheManager manager in this.DependentManagers)
+            {
+                //等10分钟
+                for (int i = 0; i < 300; i++)
+                {
+                    if (!manager.Enable)
+                    {
+                        ConsoleHelper.WriteLine(string.Format("{0}等待依赖项{1}加载数据", this.GetType().Name, manager.GetType().Name));
+                        Thread.Sleep(2000);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (!manager.Enable)
+                {
+                    throw new EIMException(string.Format("{0}等待依赖项{1}超时!", this.GetType().Name, manager.GetType().Name));
+                }
+            }
+        }
+
+        protected virtual void OnLoaded()
+        {
+            this.CacheManager.Status = CacheStatus.Enable;
+
+            this._watch.Stop();
+            int count = this.CacheManager.Count();
+            Console.WriteLine(string.Format("{0} 加载了{1}条数据, 用时: {2}", this.GetType().Name, count, this._watch.Elapsed.TotalSeconds));
+        }
+
+        protected abstract List<CacheType> GetCaches();
+
+        public abstract void Refresh(object key);
+    }
+}
